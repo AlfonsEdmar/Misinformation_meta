7924c5b61a97640522cab1dcdfe81661472bacd8
RabbitSnore <rabbitsnore@gmail.com>
2023-04-24 20:51:02 +0200

    Retrieve references using DOIs
    - Uses rcrossref to retrive bibliographic information using DOIs. This works reasonably well, but some reference information is still missing and will need to be added manually.
    - Saves an RDS file that retains the author information as a list column. The CSV file is saved with the author column as a character variable that can be parsed into a list column reasonably easily by splitting on semicolon.

--- R/misinformation_initial_cleaning.R
+++ R/misinformation_initial_cleaning.R
@@ -7,6 +7,8 @@
 # Load packages ----------------------------------------------------------------
 
 library(tidyverse)
+library(rcrossref)
+library(bib2df)
 
 # Load data --------------------------------------------------------------------
 
@@ -160,8 +162,67 @@ raw$test_medium          <- as.factor(raw$test_medium)
 raw$test_type            <- as.factor(raw$test_type)
 raw$item_centrality      <- as.factor(raw$item_centrality)
 
+# Retrieving reference information from DOIs -----------------------------------
+
+if (!file.exists("data/misinformation_citations.bib")) {
+  
+  doi_unique <- unique(raw$doi)
+  
+  citations <- cr_cn(
+    dois = doi_unique,
+    format = "bibtex",
+    style = "apa"
+  )
+  
+  writeLines(unlist(citations), "data/misinformation_citations.bib")
+  
+}
+
+citation_data <- bib2df("data/misinformation_citations.bib")
+
+colnames(citation_data) <- tolower(colnames(citation_data))
+
+citation_data$category <- tolower(citation_data$category)
+
+citation_clean <- citation_data %>% 
+  select(
+    bibtexkey, category, author, title, year, journal, doi
+  )
+
+raw_cite <- raw %>% 
+  select(
+    -title, -container, -record_type
+  ) %>% 
+  left_join(citation_clean, by = "doi") %>% 
+  select(
+    id_record, id_study, id_control, id_effect,
+    authors, publication_year,
+    bibtexkey, category, author, title, year, journal, doi,
+    everything()
+    )
+
+## Transform author list column into a character column
+
+raw <- raw %>% 
+  select(
+    -title, -container, -record_type
+  ) %>% 
+  left_join(citation_clean, by = "doi") %>% 
+  select(
+    id_record, id_study, id_control, id_effect,
+    authors, publication_year,
+    bibtexkey, category, author, title, year, journal, doi,
+    everything()
+  )
+
+raw$author <- map_chr(raw_cite$author, function(x) {paste(unlist(x), sep = " ", collapse = "; ")})
 
 # Exporting cleaned data--------------------------------------------------------
 
+# Write a csv file
+
 write.csv(raw, 'data/misinformation_data_cleaned.csv')
 
+# Write an RDS file
+
+saveRDS(raw_cite, "data/misinformation_data_cleaned.rds")
