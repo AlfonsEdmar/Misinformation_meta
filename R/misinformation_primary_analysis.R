23d55613f83de8c439f5cc5a5e6524e6817dc777
AlfonsEdmar <b.a.edmar@uu.nl>
2024-04-28 18:30:21 +0200

    uploading subgroup analysis

--- R/misinformation_primary_analysis.R
+++ R/misinformation_primary_analysis.R
@@ -303,12 +303,249 @@ I2_mv(meta_primary_outlier_2, es_data_outlier_2)
 
 funnel(meta_primary_outlier_2)
 
+# P-curve analysis -------------------------------------------------------------
+## Calculating power and p-values - we assume equal sample sizes
+power_1 <- power.t.test(n = data_es$n_control, delta = data_es$yi, 
+                        sd = sqrt(data_es$vi), sig.level = .05,
+                        type = 'two.sample')
+
+power_2 <- power.t.test(n = data_es$n_control, delta = data_es$yi, 
+                      sd = sqrt(data_es$vi), sig.level = .05,
+                      type = 'paired')
+
+data_es <- data_es %>% 
+  mutate(
+    p_value = 2 * (1- pnorm(abs(data_es$yi/sqrt(data_es$vi)))),
+    post_hoc_power  = case_when(
+      within_between=='between' ~ power_1$power,
+      within_between=='within' ~  power_2$power)
+    )
 
 
+table(high_power = ifelse(data_es$post_hoc_power > .8, 1, 0),
+      sig = ifelse(data_es$p_value < .05, 1, 0))
+# We have no high powered insignificant effects
+# We have 244 low powered insignificant effects 
+# We have 463 high powered insignificant effects
+# We have 1059 high powered significant effects
 
 
+data_es %>% ggplot(aes(y = p_value, x = post_hoc_power))+
+  geom_point(alpha = .1)+
+  geom_smooth()+
+  geom_hline(yintercept = .05)+
+  geom_vline(xintercept = .8)
+
+set.seed(123)
+data_es %>% filter(p_value < .1& p_value > .001) %>% 
+  slice_sample(by = id_record) %>% 
+  ggplot(aes(x = p_value))+
+  geom_histogram(bins = 50)
+
+# Independence of P-values are assumed. Since we have many within study effects, we can do a multiverse analysis 
+
+# Selecting variables from data_es
+study_data <- data_es %>%  slice_sample(n = 30) %>% select(id_record, id_effect, p_value)
+# Get unique study IDs
+study_data %>%  group_by(id_record) %>%  expand.grid()
 
 
+# Subgroup analyses-------------------------------------------------------------
+## Age analysis----------------------------------------------------------------- 
+age_data <- data_es %>% filter(!is.na(data_es$age_mean))
+
+meta_age   <- rma.mv(yi      = yi, 
+                     V       = vi,
+                     random  = list(~1|id_record/id_study/id_control, 
+                                        ~1|event_materials,
+                                        ~1|country,
+                                        ~1|control_type,
+                                        ~1|modality,
+                                        ~1|population,
+                                        ~1|test_type,
+                                        ~1|test_medium,
+                                        ~1|exposure_medium),
+                         mods    = ~ postevent_retention_interval
+                         + age_mean
+                         + postexposure_retention_interval
+                         + preevent_warning
+                         + postevent_warning
+                         + postexposure_warning
+                         + control_acc
+                         + postevent_recall
+                         + postexposure_recall
+                         + publication_year,
+                         data    = age_data,
+                         method  = "REML", 
+                         control = list(
+                           iter.max  = 1000,
+                           rel.tol   = 1e-8
+                         ),
+                         verbose = TRUE)
+saveRDS(meta_age, 'output/misinformation_meta_age.rds')
+
+meta_age_quad  <- rma.mv(yi      = yi, 
+                         V       = vi,
+                         random  = list(~1|id_record/id_study/id_control, 
+                                        ~1|event_materials,
+                                        ~1|country,
+                                        ~1|control_type,
+                                        ~1|modality,
+                                        ~1|population,
+                                        ~1|test_type,
+                                        ~1|test_medium,
+                                        ~1|exposure_medium),
+                         mods    = ~ postevent_retention_interval
+                         + I(age_mean^2)
+                         + postexposure_retention_interval
+                         + preevent_warning
+                         + postevent_warning
+                         + postexposure_warning
+                         + control_acc
+                         + postevent_recall
+                         + postexposure_recall
+                         + publication_year,
+                         data    = age_data,
+                         method  = "REML", 
+                         control = list(
+                           iter.max  = 1000,
+                           rel.tol   = 1e-8
+                         ),
+                         verbose = TRUE)
+saveRDS(meta_age_quad, 'output/misinformation_meta_quad_age.rds')
+
+meta_age_no_acc  <- rma.mv(yi      = yi, 
+                    V       = vi,
+                    random  = list(~1|id_record/id_study/id_control, 
+                                             ~1|event_materials,
+                                             ~1|country,
+                                             ~1|control_type,
+                                             ~1|modality,
+                                             ~1|population,
+                                             ~1|test_type,
+                                             ~1|test_medium,
+                                             ~1|exposure_medium),
+                    mods    = ~ postevent_retention_interval
+                    + age_mean
+                    + postexposure_retention_interval
+                    + preevent_warning
+                    + postevent_warning
+                    + postexposure_warning
+                    + postevent_recall
+                    + postexposure_recall
+                    + publication_year,
+                    data    = age_data,
+                    method  = "REML", 
+                    control = list(
+                      iter.max  = 1000,
+                      rel.tol   = 1e-8
+                    ),
+                    verbose = FALSE)
+saveRDS(meta_age_no_acc, 'output/misinformation_meta_age_no_accuracy.rds')
 
 
+#Categorical age
+breaks <- c(0, 5, 18, 40, max(age_data$age_mean))
+labels <- c("0-5", "6-17", "17-40", "41+")
+age_data$age_cat <- cut(age_data$age_mean, breaks = breaks, labels = labels, include.lowest = TRUE)
 
+meta_age_cat   <- rma.mv(yi      = yi, 
+                         V       = vi,
+                         random  = list(~1|id_record/id_study/id_control, 
+                                        ~1|event_materials,
+                                        ~1|country,
+                                        ~1|control_type,
+                                        ~1|modality,
+                                        ~1|population,
+                                        ~1|test_type,
+                                        ~1|test_medium,
+                                        ~1|exposure_medium),
+                         mods    = ~ postevent_retention_interval
+                         + age_cat
+                         + postexposure_retention_interval
+                         + preevent_warning
+                         + postevent_warning
+                         + postexposure_warning
+                         + control_acc
+                         + postevent_recall
+                         + postexposure_recall
+                         + publication_year,
+                         data    = age_data,
+                         method  = "REML", 
+                         control = list(
+                           iter.max  = 1000,
+                           rel.tol   = 1e-8
+                         ),
+                         verbose = F)
+saveRDS(meta_age_no_acc, 'output/misinformation_meta_age_cat.rds')
+
+## Incentives ------------------------------------------------------------------
+incent_data <- data_es %>% filter(!is.na(data_es$incentives))
+incent_data$incentives <- as.factor(incent_data$incentives)
+# Setting the reference to be no incentive
+incent_data <- within(incent_data, incentives <- relevel(incentives, 6))
+
+meta_incent<- rma.mv(yi      = yi, 
+                     V       = vi,
+                     random  = list(~1|id_record/id_study/id_control, 
+                                    ~1|event_materials,
+                                    ~1|country,
+                                    ~1|control_type,
+                                    ~1|modality,
+                                    ~1|population,
+                                    ~1|test_type,
+                                    ~1|test_medium,
+                                    ~1|exposure_medium),
+                     mods    = ~ postevent_retention_interval
+                     + incentives
+                     + postexposure_retention_interval
+                     + preevent_warning
+                     + postevent_warning
+                     + postexposure_warning
+                     + control_acc
+                     + postevent_recall
+                     + postexposure_recall
+                     + publication_year,
+                     data    = incent_data,
+                     method  = "REML", 
+                     control = list(
+                       iter.max  = 1000,
+                       rel.tol   = 1e-8
+                     ),
+                     verbose = F)
+saveRDS(meta_incent, "output/misinformation_meta_incent.rds")
+
+## Item Centrality--------------------------------------------------------------
+cent_data <- data_es %>% filter(!is.na(item_centrality))
+cent_data$item_centrality <- as.factor(cent_data$item_centrality)
+
+meta_centrality <- rma.mv(yi      = yi, 
+                          V       = vi,
+                          random  = list(~1|id_record/id_study/id_control, 
+                                    ~1|event_materials,
+                                    ~1|country,
+                                    ~1|control_type,
+                                    ~1|modality,
+                                    ~1|population,
+                                    ~1|test_type,
+                                    ~1|test_medium,
+                                    ~1|exposure_medium),
+                     mods    = ~ postevent_retention_interval
+                     + item_centrality
+                     + postexposure_retention_interval
+                     + preevent_warning
+                     + postevent_warning
+                     + postexposure_warning
+                     + control_acc
+                     + postevent_recall
+                     + postexposure_recall
+                     + publication_year,
+                     data    = cent_data,
+                     method  = "REML", 
+                     control = list(
+                       iter.max  = 1000,
+                       rel.tol   = 1e-8
+                     ),
+                     verbose = F)
+
+saveRDS(meta_centrality, "output/misinformation_meta_centrality.rds")
