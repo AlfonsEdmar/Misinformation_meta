38af553fd0546c7a95e8f967dd76978ffc7554ee
annanathalieshekila <din.email@example.com>
2024-05-31 17:37:38 +0200

    countries, incentives, year of publication

--- data/misinformation_study_characteristics.R
+++ data/misinformation_study_characteristics.R
@@ -16,12 +16,14 @@ chr <- read_csv("./data/misinformation_clean_data.csv")
 # Population characteristics ----------------------------------------------------------------
 
 # Study-level total N
+
 unique_study_n <- chr %>% distinct(id_study, .keep_all = TRUE)
 
 study_total_n <- sum(unique_study_n$n_total, na.rm = TRUE)
 print(study_total_n)
 
 # Age
+
 summary(chr$age_mean)
 summary(na.omit(chr$age_mean))
 clean_age_mean <- na.omit(chr$age_mean)
@@ -45,8 +47,31 @@ chr %>%
   summarize(total_n = sum(n_total, na.rm = TRUE), count = n(), .groups = 'drop')
 print()
 
-# Preregistration, open data & open materials
+#Population recruitment
+unique_studies <- chr %>%
+  distinct(id_study, .keep_all = TRUE)
 
+population_summary <- unique_studies %>%
+  group_by(population) %>%
+  summarize(total_n = sum(n_total, na.rm = TRUE), count = n(), .groups = 'drop')
+print(population_summary)
+
+#Incentives
+unique_incentives <- chr %>%
+  distinct(id_study, .keep_all = TRUE)
+
+incentives_study <- unique_incentives %>%
+  group_by(incentives) %>%
+  summarize(count = n(), .groups = 'drop')
+
+total_incentives <- unique_incentives %>%
+  summarize(total_count = n())
+print(incentives_study)
+
+
+# Record characteristics
+----------------------------------------------------------------
+  
 #Preregistration
 chr%>%
   filter(preregistered == 1) %>%
@@ -62,32 +87,35 @@ chr %>%
   filter(open_materials == 1) %>%
   distinct(id_study, .keep_all = TRUE)
 
-# Modality
+#Year of publication
+
+conflicting_values <- chr %>%
+  filter(!is.na(publication_year) & !is.na(year) & publication_year != year)
+print(conflicting_values)
+
+unique_records <- chr %>%
+  distinct(id_record, .keep_all = TRUE)
+
+year_record <- unique_records %>%
+  group_by(year) %>%
+  summarize(count = n(), .groups = 'drop') %>%
+  arrange(desc(count))
+print(year_record, n = Inf)
+
+
+#Modality
 unique_studies <- chr %>%
   distinct(id_study, .keep_all = TRUE)
 
-modality_study <- unique_studies %>%
+modality_study <- unique_modality %>%
   group_by(modality) %>%
   summarize(count = n(), .groups = 'drop')
 
-total_studies <- unique_studies %>%
+total_studies <- unique_modality %>%
   summarize(total_count = n())
 print(modality_study)
 
-#Population recruitment
-unique_studies <- chr %>%
-  distinct(id_study, .keep_all = TRUE)
-
-population_summary <- unique_studies %>%
-  group_by(population) %>%
-  summarize(total_n = sum(n_total, na.rm = TRUE), count = n(), .groups = 'drop')
-print(population_summary)
-
-#Incentives
-
 #Retention interval 
-
-
 
 
 
